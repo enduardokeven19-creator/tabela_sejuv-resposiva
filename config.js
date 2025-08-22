2550d7bb52206441c7780e2bc26a5062ac9a8c83
enduardokeven19-creator <enduardokeven19@gmail.com>
2025-08-22 16:47:43 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3ZGKwF8j16t5LtbibAqucUmrZykUT_BQ",
+    authDomain: "cadastro-sejuv.firebaseapp.com",
+    projectId: "cadastro-sejuv",
+    storageBucket: "cadastro-sejuv.firebasestorage.app",
+    messagingSenderId: "5134638204",
+    appId: "1:5134638204:web:a5de746c1370447ad5fdf9",
+    measurementId: "G-DHVJXVKENB"
+}
